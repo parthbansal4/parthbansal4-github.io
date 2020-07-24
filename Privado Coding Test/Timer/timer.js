874529390d21ef8d704f0434ab5a79eab63ebe06
Parth Bansal <bansal.parth4@gmail.com>
2020-07-24 09:51:36 +0530

    Update timer.js

--- Privado Coding Test/Timer/timer.js
+++ Privado Coding Test/Timer/timer.js
@@ -1,62 +1,72 @@
 var sec=0;
 var min=0;
-var displaySec=0;
-var displayMin=0;
+var timeSec=0;
+var timeMin=0;
 var interval=0;
 var isStart= false;
-var display=document.querySelector("h1");
+var time=document.querySelector("h1");
 var button=document.querySelector("#btn");
 
 function timer(){
     sec++;
-
     if(sec == 60){
         sec=0;
         min++;
     }
 
     if(sec < 10){
-        displaySec= "0" + sec.toString();
+        timeSec= "0" + sec.toString();
     }else{
-        displaySec=sec;
+        timeSec=sec;
     }
 
     if(min<10){
-        displayMin= "0"+ min.toString();
+        timeMin= "0"+ min.toString();
     }else{
-        displayMin= min;
+        timeMin= min;
     }
 
     if(min==5){
         stop();
     }
 
-    display.innerHTML= displayMin + ":" + displaySec;
+    time.innerHTML= timeMin + ":" + timeSec;
 };
 
 button.addEventListener("click", function(){
     if(button.textContent=== "Reset"){
-        interval=0;
-        sec=0;
-        min=0;
-        isStart= false;
-        button.textContent= "Start";
-        display.innerHTML= "00:00";
-        display.classList.toggle("red");
-    }else 
-    if(isStart){
-        window.clearInterval(interval);
-        button.textContent= "Start";
-        isStart= false;
+        reset();
+    }else if(isStart){
+        start();
     }else{
-        interval=window.setInterval(timer, 1000);
-        button.textContent= "Pause";
-        isStart= true;
+        pause();
     }
 });
+
+function start(){
+    window.clearInterval(interval);
+    button.textContent= "Start";
+    isStart= false;
+};
+
+function pause(){
+    interval=window.setInterval(timer, 1000);
+    button.textContent= "Pause";
+    isStart= true;
+}
 
 function stop(){
     button.textContent= "Reset";
     window.clearInterval(interval);
-    display.classList.toggle("red");
+    time.classList.toggle("red");
+};
+
+function reset(){
+    interval=0;
+    sec=0;
+    min=0;
+    isStart= false;
+    button.textContent= "Start";
+    time.innerHTML= "00:00";
+    time.classList.toggle("red");
 };
